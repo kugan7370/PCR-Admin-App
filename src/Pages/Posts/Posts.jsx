b6b4a65cdf6a7dc241a348075ff91a2f04134129
kugan18 <kugapriyanj08@gmail.com>
2022-11-30 12:47:13 +0530

    edit tables

--- src/Pages/Posts/Posts.jsx
+++ src/Pages/Posts/Posts.jsx
@@ -1,163 +1,165 @@
-import { collection, onSnapshot, orderBy, query, where } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react'
-import ArticleTable from '../../Components/Common/ArticleTable';
-import Tables from '../../Components/Common/UserTables';
-import { auth, db } from '../../Firebase'
-import { DataGrid } from '@material-ui/data-grid';
-import Moment from 'react-moment';
-import { DeleteForeverOutlined } from '@material-ui/icons';
-import { Button, Modal } from 'react-bootstrap';
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import ArticleTable from "../../Components/Common/ArticleTable";
+import Tables from "../../Components/Common/UserTables";
+import { auth, db } from "../../Firebase";
+import { DataGrid } from "@material-ui/data-grid";
+import Moment from "react-moment";
+import { DeleteForeverOutlined } from "@material-ui/icons";
+import { Button, Modal } from "react-bootstrap";
 
 function Posts() {
-    const [PostData, setPostData] = useState([]);
-    const [show, setShow] = useState(false);
+  const [PostData, setPostData] = useState([]);
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const handleDelete = (id) => {
-        console.log(id);
-    }
+  const handleDelete = (id) => {
+    console.log(id);
+  };
 
-    useEffect(() => {
-        let isMounted = true
-        const ref = collection(db, 'blogs')
-        const q = query(ref, orderBy("createAt", 'desc'))
-        const snap = onSnapshot(q, (snapshot) => {
-            let PostData = [];
-            if (isMounted) {
-                snapshot.docs.map((doc) => {
+  useEffect(() => {
+    let isMounted = true;
+    const ref = collection(db, "blogs");
+    const q = query(ref, orderBy("createAt", "desc"));
+    const snap = onSnapshot(q, (snapshot) => {
+      let PostData = [];
+      if (isMounted) {
+        snapshot.docs.map((doc) => {
+          PostData.push({ ...doc.data(), id: doc.id });
+        });
+      }
+      setPostData(PostData);
+    });
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-                    PostData.push({ ...doc.data(), id: doc.id })
-
-                })
-            }
-            setPostData(PostData)
-
-        })
-        return () => { isMounted = false }
-    }, [])
-
-    const columns = [
-        // { field: 'id', headerName: 'id', width: 90 },
-        {
-            field: 'title',
-            headerName: 'title',
-            width: 150,
-            editable: true,
-            renderCell: (params) => (
-                <div>
-                    {/* <img style={{ height: 30, width: 30, borderRadius: 10, marginRight: 10 }} src={params.row.titleImage} alt="" /> */}
-                    {params.row.title}
-                </div>
-            ),
-        },
-        {
-            field: 'title',
-            headerName: 'title',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'category',
-            headerName: 'Category',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'type',
-            headerName: 'Type',
-            width: 100,
-            editable: true,
-        },
-        {
-            field: 'user',
-            headerName: 'User',
-            width: 150,
-            editable: true,
-            renderCell: (params) => (
-                <div className="">
-                    <img style={{ height: 30, width: 30, borderRadius: 15, marginRight: 10 }} src={params.row.UserPic} alt="" />
-                    {params.row.username}
-
-                </div>
-            ),
-        },
-        {
-            field: 'usermail',
-            headerName: 'Email',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'createAt',
-            headerName: 'CreateAt',
-            width: 200,
-            editable: true,
-            renderCell: (params) => (
-                <div className="">
-                    <Moment format='LLL'>
-                        {params.row.createAt.toDate()}
-                    </Moment>
-
-
-
-                </div>
-            ),
-        },
-
-        {
-            field: 'Delete',
-            headerName: 'Delete',
-            width: 150,
-            editable: true,
-
-            renderCell: (params) => (
-                <>
-                    <Button variant='white' onClick={handleShow} className=' text-center'>
-                        <DeleteForeverOutlined className='text-danger' />
-                    </Button>
-
-                    <Modal show={show} onHide={handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Confirm</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>Are you sure to delete?</Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
-                                Close
-                            </Button>
-                            <Button variant="primary" onClick={() => handleDelete(params.row.id)}>
-                                Delete
-                            </Button>
-                        </Modal.Footer>
-                    </Modal>
-                </>
-            )
-        },
-
-
-
-    ];
-
-
-    return (
-        // <ArticleTable PostData={PostData} />
-        <div style={{ height: '95%', width: '100%', }} >
-            <h4>Posts</h4>
-            <div style={{ height: '100%', width: '100%', }}>
-                {PostData && <DataGrid
-                    rows={PostData}
-                    columns={columns}
-                    pageSize={7}
-                    checkboxSelection
-                    disableSelectionOnClick
-
-                />}
-            </div>
+  const columns = [
+    // { field: 'id', headerName: 'id', width: 90 },
+    {
+      field: "title",
+      headerName: "title",
+      width: 150,
+      editable: true,
+      renderCell: (params) => (
+        <div>
+          {/* <img style={{ height: 30, width: 30, borderRadius: 10, marginRight: 10 }} src={params.row.titleImage} alt="" /> */}
+          {params.row.title}
         </div>
+      ),
+    },
+    {
+      field: "title",
+      headerName: "title",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "category",
+      headerName: "Category",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "type",
+      headerName: "Type",
+      width: 100,
+      editable: true,
+    },
+    {
+      field: "user",
+      headerName: "User",
+      width: 150,
+      editable: true,
+      renderCell: (params) => (
+        <div className="">
+          <img
+            style={{ height: 30, width: 30, borderRadius: 15, marginRight: 10 }}
+            src={params.row.UserPic}
+            alt=""
+          />
+          {params.row.username}
+        </div>
+      ),
+    },
+    {
+      field: "usermail",
+      headerName: "Email",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "createAt",
+      headerName: "CreateAt",
+      width: 200,
+      editable: true,
+      renderCell: (params) => (
+        <div className="">
+          <Moment format="LLL">{params.row.createAt.toDate()}</Moment>
+        </div>
+      ),
+    },
 
-    )
+    {
+      field: "Delete",
+      headerName: "Delete",
+      width: 150,
+      editable: true,
+
+      renderCell: (params) => (
+        <>
+          <Button variant="white" onClick={handleShow} className="text-center">
+            <DeleteForeverOutlined className="text-danger" />
+          </Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Confirm</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Are you sure to delete?</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button
+                variant="primary"
+                onClick={() => handleDelete(params.row.id)}
+              >
+                Delete
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+      ),
+    },
+  ];
+
+  return (
+    // <ArticleTable PostData={PostData} />
+    <div className="mt-2" style={{ height: "95%", width: "100%" }}>
+      <h4>Posts</h4>
+      <div style={{ height: "100%", width: "100%" }}>
+        {PostData && (
+          <DataGrid
+            rows={PostData}
+            columns={columns}
+            pageSize={7}
+            checkboxSelection
+            disableSelectionOnClick
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Posts
+export default Posts;
--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -1,95 +1,91 @@
-import { collection, onSnapshot, query, where } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react'
-import UserTables from '../../Components/Common/UserTables';
-import { DataGrid } from '@material-ui/data-grid';
-import { auth, db } from '../../Firebase'
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import UserTables from "../../Components/Common/UserTables";
+import { DataGrid } from "@material-ui/data-grid";
+import { auth, db } from "../../Firebase";
 
 function User() {
-    const [userData, setuserData] = useState([]);
+  const [userData, setuserData] = useState([]);
 
-    useEffect(() => {
-        let isMounted = true
-        const ref = collection(db, 'users')
-        const snap = onSnapshot(ref, (snapshot) => {
-            let userData = [];
-            if (isMounted) {
-                snapshot.docs.map((doc) => {
+  useEffect(() => {
+    let isMounted = true;
+    const ref = collection(db, "users");
+    const snap = onSnapshot(ref, (snapshot) => {
+      let userData = [];
+      if (isMounted) {
+        snapshot.docs.map((doc) => {
+          userData.push({ ...doc.data(), id: doc.id });
+        });
+      }
+      setuserData(userData);
+    });
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-                    userData.push({ ...doc.data(), id: doc.id })
+  const columns = [
+    { field: "id", headerName: "ID", width: 400 },
+    {
+      field: "user",
+      headerName: "Username",
+      width: 250,
 
-                })
-            }
-            setuserData(userData)
-
-        })
-        return () => { isMounted = false }
-    }, [])
-
-
-
-    const columns = [
-        { field: 'id', headerName: 'id', width: 90, },
-        {
-            field: 'user',
-            headerName: 'username',
-            width: 150,
-
-            renderCell: (params) => (
-                <div className="">
-                    <img style={{ height: 30, width: 30, borderRadius: 15, marginRight: 10 }} src={params.row.pro_pic} alt="" />
-                    {params.row.username}
-
-                </div>
-            ),
-
-
-
-            editable: true,
-
-        },
-
-        {
-            field: 'email',
-            headerName: 'email',
-            width: 150,
-            editable: true,
-        },
-
-    ];
-
-    // const rows = [
-    //     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    //     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    //     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    //     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    //     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    //     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    //     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    //     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    //     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    // ];
-
-
-
-    return (
-        <div className=" d-flex justify-content-lg-around mt-4">
-
-            <div style={{ height: 400, width: '50%', }}>
-                {userData && <DataGrid
-                    rows={userData}
-                    columns={columns}
-                    pageSize={5}
-                    checkboxSelection
-                    disableSelectionOnClick
-                />}
-            </div>
-
-            <div className="user-analysis-image">
-                <img src="https://i0.wp.com/www.rwdigital.ca/wp-content/uploads/2022/03/Search-Engine-Optimization-SEO.png?fit=421%2C295&ssl=1" height="350px" alt="" srcset="" />
-            </div>
+      renderCell: (params) => (
+        <div className="">
+          <img
+            style={{ height: 30, width: 30, borderRadius: 15, marginRight: 20 }}
+            src={params.row.pro_pic}
+            alt=""
+          />
+          {params.row.username}
         </div>
-        // <UserTables userData={userData} />
-    )
+      ),
+
+      editable: true,
+    },
+
+    {
+      field: "email",
+      headerName: "Email",
+      width: 350,
+      editable: true,
+    },
+  ];
+
+  // const rows = [
+  //     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  //     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  //     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  //     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  //     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  //     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  //     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  //     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  //     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  // ];
+
+  return (
+    <div className="mt-2" style={{ height: "95%", width: "100%" }}>
+      <h4>Users</h4>
+      <div style={{ height: "100%", width: "100%" }}>
+        {userData && (
+          <DataGrid
+            rows={userData}
+            columns={columns}
+            pageSize={5}
+            checkboxSelection
+            disableSelectionOnClick
+          />
+        )}
+      </div>
+
+      {/* <div className="user-analysis-image">
+                <img src="https://i0.wp.com/www.rwdigital.ca/wp-content/uploads/2022/03/Search-Engine-Optimization-SEO.png?fit=421%2C295&ssl=1" height="350px" alt="" srcset="" />
+            </div> */}
+    </div>
+    // <UserTables userData={userData} />
+  );
 }
 
-export default User
+export default User;
