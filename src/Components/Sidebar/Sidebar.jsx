113c560ac14d2eb6291fbd13262c546ddffd2ca8
kugan18 <kugapriyanj08@gmail.com>
2022-12-07 14:01:03 +0530

    updated dashboad

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,52 +1,85 @@
-import { AccountBox, Dashboard, ExitToApp, Group, MenuBook } from '@material-ui/icons'
-import { signOut } from 'firebase/auth'
-import React from 'react'
-import { Nav } from 'react-bootstrap'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import {
+  AccountBox,
+  Dashboard,
+  ExitToApp,
+  Group,
+  MenuBook,
+} from "@material-ui/icons";
+import { signOut } from "firebase/auth";
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-import { auth } from '../../Firebase'
-import './Sidebar.css'
+import { auth } from "../../Firebase";
+import "./Sidebar.css";
 export default function Sidebar() {
-    const userData = useSelector((state) => state.authUser.user);
+  const userData = useSelector((state) => state.authUser.user);
 
-    const userSignout = () => {
-        signOut(auth);
-    }
-    return (
-        <div className='sidebar  p-4'>
+  const userSignout = () => {
+    signOut(auth);
+  };
+  return (
+    <div className="sidebar  p-4">
+      <div className="profile-details  align-items-center d-flex flex-column mb-5">
+        <img
+          src={userData.pro_pic}
+          className="pro-image rounded-circle"
+          alt=""
+        />
+        <span className="mt-1">{userData.username}</span>
+      </div>
+      <Nav defaultActiveKey="/home" className="flex-column text-white">
+        <Link
+          className="d-flex align-items-center text-white mb-2 text-decoration-none "
+          to="/dashboard"
+        >
+          <Dashboard className=" fs-5 me-2  " />
+          <h6 className="mt-2 active">Dashboard</h6>
+        </Link>
 
-            <div className="profile-details  align-items-center d-flex flex-column mb-5">
-                <img src={userData.pro_pic} className='pro-image rounded-circle' alt="" />
-                <span className='mt-1'>{userData.username}</span>
-            </div>
-            <Nav defaultActiveKey="/home" className="flex-column text-white">
-                <Link className="d-flex align-items-center text-white mb-2   text-decoration-none " to="/dashboard">
-                    <Dashboard className=' fs-5 me-2  ' />
-                    <h6 className='mt-2 active'>Dashboard</h6>
-                </Link>
+        <Link
+          className="d-flex align-items-center text-white mb-2   text-decoration-none"
+          to="/Posts"
+        >
+          <MenuBook className=" fs-5 me-2" />
+          <h6 className="mt-2">Posts</h6>
+        </Link>
 
-                <Link className="d-flex align-items-center text-white mb-2   text-decoration-none" to="/Posts">
-                    <MenuBook className=' fs-5 me-2' />
-                    <h6 className='mt-2'>Posts</h6>
-                </Link>
+        <Link
+          to="/users"
+          className="d-flex align-items-center mb-5 text-white text-decoration-none "
+        >
+          <Group className=" fs-5 me-2" />
+          <h6 className="mt-2">Users</h6>
+        </Link>
+        <Link
+          to="/users"
+          className="d-flex align-items-center mb-2  text-white text-decoration-none imageContainer "
+        >
+          {/* <Group className=" fs-5 me-2" /> */}
+          <img
+            src="./images/Data1.png"
+            className="rounded-circle   img-fluid"
+            alt=""
+          />
+        </Link>
 
-                <Link to="/users" className="d-flex align-items-center mb-2 text-white text-decoration-none ">
-                    <Group className=' fs-5 me-2' />
-                    <h6 className='mt-2'>Users</h6>
-                </Link>
-
-                <Link className="d-flex align-items-center text-white  mb-5 text-decoration-none" to="/Profile">
+        {/* <Link className="d-flex align-items-center text-white  mb-5 text-decoration-none" to="/Profile">
                     <AccountBox className=' fs-5 me-2' />
                     <h6 className='mt-2'>Profile</h6>
-                </Link>
+                </Link> */}
 
-                <Nav.Link className="d-flex align-items-center text-white mt-5 text-decoration-none p-0" href="/" >
-                    <ExitToApp className=' fs-5 me-2' />
-                    <h6 className='mt-2' onClick={userSignout}>Logout</h6>
-                </Nav.Link>
-
-            </Nav>
-        </div>
-    )
+        <Nav.Link
+          className="d-flex align-items-center text-white mt-4  text-decoration-none p-0"
+          href="/"
+        >
+          <ExitToApp className=" fs-5 me-2" />
+          <h6 className="mt-2" onClick={userSignout}>
+            Logout
+          </h6>
+        </Nav.Link>
+      </Nav>
+    </div>
+  );
 }
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,225 +1,186 @@
-import { Assessment, ChromeReaderMode, Group, LocalLibrary, MenuBook } from '@material-ui/icons'
-import { collection, onSnapshot, query, where } from 'firebase/firestore'
-import React, { useEffect, useState } from 'react'
-import { Card, Col, Row } from 'react-bootstrap'
-import { db } from '../../Firebase'
-import './Dashboard.css'
+import {
+  Assessment,
+  ChromeReaderMode,
+  Group,
+  LocalLibrary,
+  MenuBook,
+} from "@material-ui/icons";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { Card, Col, Row } from "react-bootstrap";
+import { db } from "../../Firebase";
+import "./Dashboard.css";
 function Dashboard() {
-    const [user, setuser] = useState()
-    const [post, setpost] = useState()
-    const [article, setarticle] = useState()
-    const [book, setbook] = useState()
-    //user-count
+  const [user, setuser] = useState();
+  const [post, setpost] = useState();
+  const [article, setarticle] = useState();
+  const [book, setbook] = useState();
+  //user-count
 
-    useEffect(() => {
-        let isMounted = true
-        try {
+  useEffect(() => {
+    let isMounted = true;
+    try {
+      const ref = collection(db, "users");
 
-            const ref = collection(db, 'users')
+      const snapdata = onSnapshot(ref, (snapshot) => {
+        let user = [];
+        if (isMounted) {
+          snapshot.docs.map((doc) => {
+            user.push({ ...doc.data(), id: doc.id });
+          });
 
-
-            const snapdata = onSnapshot(ref, (snapshot) => {
-
-                let user = [];
-                if (isMounted) {
-                    snapshot.docs.map((doc) => {
-
-                        user.push({ ...doc.data(), id: doc.id })
-                    })
-
-                    setuser(user)
-
-                }
-
-            })
-
-        } catch (error) {
-
-            let user = [];
-            setuser(user)
-
+          setuser(user);
         }
+      });
+    } catch (error) {
+      let user = [];
+      setuser(user);
+    }
 
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-        return () => { isMounted = false }
-    }, [])
+  //post-count
 
-    //post-count
+  useEffect(() => {
+    let isMounted = true;
+    try {
+      const ref = collection(db, "blogs");
 
+      const snapdata = onSnapshot(ref, (snapshot) => {
+        let post = [];
+        if (isMounted) {
+          snapshot.docs.map((doc) => {
+            post.push({ ...doc.data(), id: doc.id });
+          });
 
-
-
-    useEffect(() => {
-        let isMounted = true
-        try {
-
-            const ref = collection(db, 'blogs')
-
-
-            const snapdata = onSnapshot(ref, (snapshot) => {
-
-                let post = [];
-                if (isMounted) {
-                    snapshot.docs.map((doc) => {
-
-                        post.push({ ...doc.data(), id: doc.id })
-                    })
-
-                    setpost(post)
-
-                }
-
-            })
-
-        } catch (error) {
-
-            let post = [];
-            setpost(post)
-
+          setpost(post);
         }
+      });
+    } catch (error) {
+      let post = [];
+      setpost(post);
+    }
 
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-        return () => { isMounted = false }
-    }, [])
+  //article-count
+  useEffect(() => {
+    let isMounted = true;
+    try {
+      const ref = collection(db, "blogs");
 
+      const q = query(ref, where("type", "==", "Article"));
+      const snapdata = onSnapshot(q, (snapshot) => {
+        let article = [];
+        if (isMounted) {
+          snapshot.docs.map((doc) => {
+            article.push({ ...doc.data(), id: doc.id });
+          });
 
-    //article-count
-    useEffect(() => {
-        let isMounted = true
-        try {
-
-            const ref = collection(db, 'blogs')
-
-            const q = query(ref, where("type", '==', "Article"))
-            const snapdata = onSnapshot(q, (snapshot) => {
-
-                let article = [];
-                if (isMounted) {
-                    snapshot.docs.map((doc) => {
-
-                        article.push({ ...doc.data(), id: doc.id })
-                    })
-
-                    setarticle(article)
-
-                }
-
-            })
-
-        } catch (error) {
-
-            let article = [];
-            setarticle(article)
-
+          setarticle(article);
         }
+      });
+    } catch (error) {
+      let article = [];
+      setarticle(article);
+    }
 
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-        return () => { isMounted = false }
-    }, [])
+  //book-count
+  useEffect(() => {
+    let isMounted = true;
+    try {
+      const ref = collection(db, "blogs");
 
-    //book-count
-    useEffect(() => {
-        let isMounted = true
-        try {
+      const q = query(ref, where("type", "==", "Book"));
+      const snapdata = onSnapshot(q, (snapshot) => {
+        let book = [];
+        if (isMounted) {
+          snapshot.docs.map((doc) => {
+            book.push({ ...doc.data(), id: doc.id });
+          });
 
-            const ref = collection(db, 'blogs')
-
-            const q = query(ref, where("type", '==', "Book"))
-            const snapdata = onSnapshot(q, (snapshot) => {
-
-                let book = [];
-                if (isMounted) {
-                    snapshot.docs.map((doc) => {
-
-                        book.push({ ...doc.data(), id: doc.id })
-                    })
-
-                    setbook(book)
-
-                }
-
-            })
-
-        } catch (error) {
-
-            let book = [];
-            setbook(book)
-
+          setbook(book);
         }
+      });
+    } catch (error) {
+      let book = [];
+      setbook(book);
+    }
 
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-        return () => { isMounted = false }
-    }, [])
+  return (
+    <div>
+      <Row className="p-2">
+        <Col lg={3}>
+          <Card className="card-1 p-2 ">
+            <Card.Body>
+              <Card.Title>Users</Card.Title>
+              <Card.Text className=" d-flex align-items-center">
+                <Group className="fs-1" />
+                {user && <span className="ms-2 fs-3">{user.length}</span>}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col lg={3}>
+          <Card className="card-2 p-2">
+            <Card.Body>
+              <Card.Title>Posts</Card.Title>
+              <Card.Text className=" d-flex align-items-center">
+                <LocalLibrary className="fs-1" />
+                {post && <span className="ms-2 fs-3">{post.length}</span>}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col lg={3}>
+          <Card className="card-3 p-2">
+            <Card.Body>
+              <Card.Title>Article</Card.Title>
+              <Card.Text className=" d-flex align-items-center">
+                <Assessment className="fs-1" />
+                {article && <span className="ms-2 fs-3">{article.length}</span>}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col lg={3}>
+          <Card className="card-4 p-2">
+            <Card.Body>
+              <Card.Title>Books</Card.Title>
+              <Card.Text className=" d-flex align-items-center">
+                <MenuBook className="fs-1" />
+                {book && <span className="ms-2 fs-3">{book.length}</span>}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
 
-
-
-    return (
-        <div>
-            <Row className='p-2'>
-                <Col lg={3}>
-                    <Card className='card-1 p-2 '>
-
-                        <Card.Body>
-                            <Card.Title>Users</Card.Title>
-                            <Card.Text className=' d-flex align-items-center'>
-                                <Group className='fs-1' />
-                                {user && <span className='ms-2 fs-3'>{user.length}</span>}
-                            </Card.Text>
-                        </Card.Body>
-
-                    </Card>
-                </Col>
-                <Col lg={3}>
-                    <Card className='card-2 p-2'>
-
-
-                        <Card.Body>
-                            <Card.Title>Posts</Card.Title>
-                            <Card.Text className=' d-flex align-items-center'>
-                                <LocalLibrary className='fs-1' />
-                                {post && <span className='ms-2 fs-3'>{post.length}</span>}
-                            </Card.Text>
-                        </Card.Body>
-
-                    </Card>
-                </Col>
-                <Col lg={3}>
-                    <Card className='card-3 p-2'>
-
-
-                        <Card.Body>
-                            <Card.Title>Article</Card.Title>
-                            <Card.Text className=' d-flex align-items-center'>
-                                <Assessment className='fs-1' />
-                                {article && <span className='ms-2 fs-3'>{article.length}</span>}
-                            </Card.Text>
-                        </Card.Body>
-
-                    </Card>
-                </Col>
-                <Col lg={3}>
-                    <Card className='card-4 p-2'>
-
-
-                        <Card.Body>
-                            <Card.Title>Books</Card.Title>
-                            <Card.Text className=' d-flex align-items-center'>
-                                <MenuBook className='fs-1' />
-                                {book && <span className='ms-2 fs-3'>{book.length}</span>}
-                            </Card.Text>
-                        </Card.Body>
-
-                    </Card>
-                </Col>
-
-            </Row>
-
-            <div className="dashboad-image mt-4 text-center ">
-                <img src="https://media-exp1.licdn.com/dms/image/C5612AQEyn_QuJGIvWA/article-cover_image-shrink_720_1280/0/1629356866985?e=1652918400&v=beta&t=HbTHqs44Kp0bNzt3VCnJyIvZ1O_93lJxJN_chxB0BSI" class="img-fluid d-image" alt="" />
-            </div>
-        </div>
-
-    )
+      <div className="dashboad-image mt-4 text-center ">
+        <img
+          src="https://www.enable.services/wp-content/uploads/2020/10/banner-bg-e1603925229997-1200x821.png"
+          class="img-fluid d-image"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
